36569dbb0c987b6059ad889edc85cf82d45092a0
Prasanna Hegade <prasann.2003hegade@gmail.com>
2026-05-14 01:30:36 +0530

    final stage upgradation

--- backend/server.js
+++ backend/server.js
@@ -9,44 +9,32 @@ const paymentRoutes = require("./routes/payment");
 
 const app = express();
 
-
-//  CORS FIX
 app.use(
   cors({
     origin: "https://spotify-frontend-tjpx.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-//  IMPORTANT (preflight fix)
-app.options("*", cors());
-
-
-//  Middlewares
 app.use(express.json());
+
 app.use("/uploads", express.static("uploads"));
 
-
-//  Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/payment", paymentRoutes);
 
-
-//  MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-
-// Test Route
 app.get("/", (req, res) => {
   res.send("Welcome to the Spotify backend!");
 });
 
-
-// Server Start
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
